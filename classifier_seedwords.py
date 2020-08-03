d1dabc03fa354da564cfbb5f36b87859c2763bc3
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-08-03 02:36:55 -0700

    Added word2vec and classifier codes.

--- classifier_seedwords.py
+++ classifier_seedwords.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import classification_report
 from nltk.corpus import stopwords
+from gensim.models import word2vec
 import string
 import numpy as np
 from util import *
@@ -9,10 +10,28 @@ import random
 import torch
 from transformers import BertModel, BertTokenizer
 from sklearn.linear_model import LogisticRegression
-
+import nltk
 import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+
+def get_word_embedding(word):
+    global embedding_model
+    try:
+        temp = embedding_model[word]
+        return temp
+    except:
+        temp = word.split()
+        vec = np.zeros(embedding_model.vector_size)
+        for w in temp:
+            try:
+                vec += embedding_model[w]
+            except:
+                print("Unable to find embedding for ", w)
+                vec += np.random.uniform(-0.25, 0.25, embedding_model.vector_size)
+        vec = vec / len(temp)
+        return vec
 
 
 def get_bert_embedding(word):
@@ -28,6 +47,13 @@ def get_bert_embedding(word):
     hidden_states = out[2]
     sentence_embedding = torch.mean(hidden_states[-1], dim=1).squeeze()
     return sentence_embedding
+
+
+def get_embeddings(word, sim="bert"):
+    if sim == "bert":
+        return get_bert_embedding(word).detach().cpu().numpy()
+    else:
+        return get_word_embedding(word)
 
 
 def preprocess_df(df):
@@ -49,7 +75,7 @@ def preprocess_df(df):
     return df
 
 
-def get_rank_matrix(docfreq, inv_docfreq, label_docs_dict, doc_freq_thresh=5):
+def get_rank_matrix(docfreq, inv_docfreq, label_docs_dict, doc_freq_thresh=5, sim=None):
     stop_words = set(stopwords.words('english'))
     stop_words.add('would')
     components = {}
@@ -68,13 +94,19 @@ def get_rank_matrix(docfreq, inv_docfreq, label_docs_dict, doc_freq_thresh=5):
                     continue
             except:
                 continue
-            components[l][name] = {
-                "reldocfreq": docfreq_local[name] / docfreq[name],
-                "idf": inv_docfreq[name],
-                "rel_freq": np.tanh(rel_freq[i]),
-                "similarity": cosine_similarity(get_bert_embedding(l).detach().cpu().numpy(),
-                                                get_bert_embedding(name).detach().cpu().numpy())
-            }
+            if sim is None:
+                components[l][name] = {
+                    "reldocfreq": docfreq_local[name] / docfreq[name],
+                    "idf": inv_docfreq[name],
+                    "rel_freq": np.tanh(rel_freq[i])
+                }
+            else:
+                components[l][name] = {
+                    "reldocfreq": docfreq_local[name] / docfreq[name],
+                    "idf": inv_docfreq[name],
+                    "rel_freq": np.tanh(rel_freq[i]),
+                    "similarity": cosine_similarity(get_embeddings(l, sim), get_embeddings(name, sim))
+                }
     return components
 
 
@@ -101,8 +133,7 @@ def compute_on_demand_feature(df, parent_label, child_label_str, sim=None):
     if sim is None:
         return [reldocfreq, idf, rel_freq]
     else:
-        similarity = cosine_similarity(get_bert_embedding(parent_label).detach().cpu().numpy(),
-                                       get_bert_embedding(child_label_str).detach().cpu().numpy())
+        similarity = cosine_similarity(get_embeddings(parent_label, sim), get_embeddings(child_label_str, sim))
 
         return [reldocfreq, idf, rel_freq, similarity]
 
@@ -128,16 +159,29 @@ def get_feature(df, parent_label, child_label, components, sim=None):
 def generate_negative_features(parent_label, parent_to_child, components, df):
     stop_words = set(stopwords.words('english'))
     stop_words.add('would')
-    vect = CountVectorizer(tokenizer=lambda x: x.split(), stop_words=stop_words, min_df=5)
+    is_noun = lambda pos: pos[:2] == 'NN'
 
     pos_df = df[df.label.isin([parent_label])]
-    X = vect.fit_transform(pos_df["text"])
-    in_vocab = vect.get_feature_names()
-    in_vocab = list(set(in_vocab) - set(parent_to_child[parent_label]))
+    in_vocab = set()
+    for sent in pos_df.text:
+        words = sent.strip().split()
+        nouns = set([word for (word, pos) in nltk.pos_tag(words) if is_noun(pos)])
+        for n in nouns:
+            if len(n) <= 2:
+                continue
+            in_vocab.add(n)
+    in_vocab = list(in_vocab - set(parent_to_child[parent_label]))
 
     neg_df = df[~df.label.isin([parent_label])]
-    X = vect.fit_transform(neg_df["text"])
-    out_vocab = vect.get_feature_names()
+    out_vocab = set()
+    for sent in neg_df.text:
+        words = sent.strip().split()
+        nouns = set([word for (word, pos) in nltk.pos_tag(words) if is_noun(pos)])
+        for n in nouns:
+            if len(n) <= 2:
+                continue
+            out_vocab.add(n)
+    out_vocab = list(out_vocab)
 
     features = []
     labels = []
@@ -150,7 +194,6 @@ def generate_negative_features(parent_label, parent_to_child, components, df):
         # if 0.75 < p < 1 two negative examples, one from in vocab and one from out vocab
 
         p = random.uniform(0, 1)
-
         if p < 0.25:
             neg_child_label = random.choice(in_vocab)
             temp_feature = get_feature(df, parent_label, neg_child_label, components)
@@ -196,8 +239,13 @@ if __name__ == "__main__":
     dataset = "nyt"
     data_path = base_path + dataset + "/"
 
-    bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    bert_model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+    sim = None
+
+    if sim is not None and sim == "bert":
+        bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        bert_model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+    elif sim is not None and sim == "word2vec":
+        embedding_model = word2vec.Word2Vec.load(data_path + "word2vec.model")
 
     df = pickle.load(open(data_path + "df_coarse.pkl", "rb"))
     child_to_parent = pickle.load(open(data_path + "child_to_parent.pkl", "rb"))
@@ -208,14 +256,20 @@ if __name__ == "__main__":
     inv_docfreq = calculate_inv_doc_freq(df, docfreq)
     label_docs_dict = get_label_docs_dict(df)
 
-    # components = get_rank_matrix(docfreq, inv_docfreq, label_docs_dict, doc_freq_thresh=5)
-    components = pickle.load(open(data_path + "components.pkl", "rb"))
+    components = get_rank_matrix(docfreq, inv_docfreq, label_docs_dict, doc_freq_thresh=5, sim=sim)
+    if sim is None:
+        pickle.dump(components, open(data_path + "model_dumps/components.pkl", "wb"))
+    elif sim == "bert":
+        pickle.dump(components, open(data_path + "model_dumps/components_bert.pkl", "wb"))
+    elif sim == "word2vec":
+        pickle.dump(components, open(data_path + "model_dumps/components_word2vec.pkl", "wb"))
+    # components = pickle.load(open(data_path + "components.pkl", "rb"))
     features = []
     seed_labels = []
 
     for parent_label in parent_to_child:
         for child_label in parent_to_child[parent_label]:
-            temp_feature = get_feature(df, parent_label, child_label, components, sim=None)
+            temp_feature = get_feature(df, parent_label, child_label, components, sim=sim)
             if len(temp_feature) > 0:
                 features.append(temp_feature)
                 seed_labels.append(1)
@@ -235,4 +289,9 @@ if __name__ == "__main__":
     preds = clf.predict(feature_vec)
     print("Training Performance")
     print(classification_report(seed_labels, preds))
-    pickle.dump(clf, open(data_path + "model_dumps/clf_logreg_nosim.pkl", "wb"))
+    if sim is None:
+        pickle.dump(clf, open(data_path + "model_dumps/clf_logreg_nosim.pkl", "wb"))
+    elif sim == "bert":
+        pickle.dump(clf, open(data_path + "model_dumps/clf_logreg_bert.pkl", "wb"))
+    elif sim == "word2vec":
+        pickle.dump(clf, open(data_path + "model_dumps/clf_logreg_word2vec.pkl", "wb"))
---
+++ word2vec.py
@@ -0,0 +1,68 @@
+import pickle
+from gensim.models import word2vec
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from classifier_seedwords import preprocess_df
+
+
+def fit_get_tokenizer(data, max_words):
+    tokenizer = Tokenizer(num_words=max_words, filters='!"#%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n')
+    tokenizer.fit_on_texts(data)
+    return tokenizer
+
+
+def train_word2vec(df, dataset_path):
+    def get_embeddings(inp_data, vocabulary_inv, size_features=100,
+                       mode='skipgram',
+                       min_word_count=2,
+                       context=5):
+        num_workers = 15  # Number of threads to run in parallel
+        downsampling = 1e-3  # Downsample setting for frequent words
+        print('Training Word2Vec model...')
+        sentences = [[vocabulary_inv[w] for w in s] for s in inp_data]
+        if mode == 'skipgram':
+            sg = 1
+            print('Model: skip-gram')
+        elif mode == 'cbow':
+            sg = 0
+            print('Model: CBOW')
+        embedding_model = word2vec.Word2Vec(sentences, workers=num_workers,
+                                            sg=sg,
+                                            size=size_features,
+                                            min_count=min_word_count,
+                                            window=context,
+                                            sample=downsampling)
+        embedding_model.init_sims(replace=True)
+        return embedding_model
+        # embedding_weights = np.zeros((len(vocabulary_inv) + 1, size_features))
+        # embedding_weights[0] = 0
+        # for i, word in vocabulary_inv.items():
+        #     if word in embedding_model:
+        #         embedding_weights[i] = embedding_model[word]
+        #     else:
+        #         embedding_weights[i] = np.random.uniform(-0.25, 0.25, embedding_model.vector_size)
+        #
+        # return embedding_weights
+
+    tokenizer = fit_get_tokenizer(df.text, max_words=150000)
+    print("Total number of words: ", len(tokenizer.word_index))
+    tagged_data = tokenizer.texts_to_sequences(df.text)
+    vocabulary_inv = {}
+    for word in tokenizer.word_index:
+        vocabulary_inv[tokenizer.word_index[word]] = word
+    embedding_model = get_embeddings(tagged_data, vocabulary_inv)
+    pickle.dump(embedding_model, open(dataset_path + "word2vec.model", "wb"))
+
+
+if __name__ == "__main__":
+    import os
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    base_path = "/data4/dheeraj/coarse2fine/"
+    # base_path = "/Users/dheerajmekala/Work/Coarse2Fine/data/"
+    dataset = "nyt"
+    data_path = base_path + dataset + "/"
+
+    df = pickle.load(open(data_path + "df_coarse.pkl", "rb"))
+    df = preprocess_df(df)
+    train_word2vec(df, data_path)
