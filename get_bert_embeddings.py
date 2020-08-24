fd263e093783786d319ae4273297bf133ac7d39b
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-08-24 00:13:42 -0700

    Added zihan bert embeddings

--- get_bert_embeddings.py
+++ get_bert_embeddings.py
@@ -9,6 +9,48 @@ import numpy as np
 os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 
 
+def get_zihan_bert_embeddings(embeddings, count, model, tokenizer, sentences):
+    def tensor_to_numpy(tensor):
+        return tensor.clone().detach().cpu().numpy()
+
+    max_tokens = 512 - 2
+    layer = 12
+    for sentence in sentences:
+        tokenized_text = tokenizer.basic_tokenizer.tokenize(sentence, never_split=tokenizer.all_special_tokens)
+        _tokenized_text = []
+        tokenized_to_id_indicies = []
+        check_tokens_id_list = []
+        cur_id_len = 0
+        for index, token in enumerate(tokenized_text):
+            tokens = tokenizer.wordpiece_tokenizer.tokenize(token)
+            if cur_id_len + len(tokens) <= max_tokens:
+                _tokenized_text.append(token)
+                tokenized_to_id_indicies.append((cur_id_len, cur_id_len + len(tokens)))
+                cur_id_len += len(tokens)
+                check_tokens_id_list.extend(tokenizer.convert_tokens_to_ids(tokens))
+            else:
+                break
+        tokenized_text = _tokenized_text
+        del _tokenized_text
+        tokens_id = tokenizer.encode(" ".join(tokenized_text), add_special_tokens=True)
+        assert tokens_id[1: -1] == check_tokens_id_list
+        input_ids = torch.tensor([tokens_id], device=model.device)
+        with torch.no_grad():
+            hidden_states = model(input_ids)
+        all_layer_outputs = hidden_states[2]
+        last_layer = tensor_to_numpy(all_layer_outputs[layer].squeeze(0))[1: -1]
+        for text, (start_index, end_index) in zip(tokenized_text, tokenized_to_id_indicies):
+            word_vec = np.average(last_layer[start_index: end_index], axis=0)
+            try:
+                count[text] += 1
+                embeddings[text] += word_vec
+            except:
+                count[text] = 1
+                embeddings[text] = word_vec
+
+    return embeddings, count
+
+
 def get_bert_embeddings(embeddings, count, model, tokenizer, sentences):
     # concatenate last 4 layers for the word embedding
     batch_word_ids = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
@@ -71,6 +113,6 @@ if __name__ == "__main__":
     i = 0
     for batch in chunks(list(df.text), n=50):
         print("Batch: ", i)
-        embeddings, count = get_bert_embeddings(embeddings, count, model, tokenizer, batch)
+        embeddings, count = get_zihan_bert_embeddings(embeddings, count, model, tokenizer, batch)
         i += 1
     pickle.dump(embeddings, open(pkl_dump_dir + "bert_word_embeddings.pkl", "wb"))
