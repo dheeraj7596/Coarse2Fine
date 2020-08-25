50d8e25ef67a26420b189fa93f9aa1fdc1af52e0
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-08-25 00:57:10 -0700

    label specific bert embeddings added.

--- get_label_specific_bert_embeddings.py
+++ get_label_specific_bert_embeddings.py
@@ -51,40 +51,6 @@ def get_zihan_bert_embeddings(embeddings, count, model, tokenizer, sentences):
     return embeddings, count
 
 
-def get_bert_embeddings(embeddings, count, model, tokenizer, sentences):
-    # concatenate last 4 layers for the word embedding
-    batch_word_ids = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    batch_word_ids = batch_word_ids.to(device)
-    out = model(**batch_word_ids)
-    hidden_states = out[2]  # tuple of size num_attn heads = 13, each containing batch_size x num_tokens x dim
-    batch_embeddings = torch.stack(hidden_states, dim=0)  # (13 x batch_size x num_tokens x dim)
-    batch_embeddings = batch_embeddings.permute(1, 2, 0, 3)  # (batch_size x num_tokens x 13 x dim)
-    for i, sent in enumerate(sentences):
-        sentence_embedding = batch_embeddings[i, :, :, :]  # (num_tokens x 13 x dim)
-        total_tokens = sentence_embedding.shape[0]
-        words = sent.strip().split()
-        ind = 0
-        for j, word in enumerate(words):
-            num_tokens = len(tokenizer.tokenize(word))
-            if ind >= total_tokens or ind + num_tokens > total_tokens:
-                break
-            word_embeddings = sentence_embedding[ind: ind + num_tokens, -4:, :]  # (num_tokens x 4 x dim)
-            word_embeddings = word_embeddings.contiguous().view(num_tokens, -1)
-            word_embeddings = torch.mean(word_embeddings, dim=0)
-            ind += num_tokens
-            try:
-                embeddings[word] += word_embeddings.contiguous().detach().cpu().numpy()
-                count[word] += 1
-            except:
-                embeddings[word] = word_embeddings.contiguous().detach().cpu().numpy()
-                count[word] = 1
-
-    for word in embeddings:
-        embeddings[word] = embeddings[word] / count[word]
-    return embeddings, count
-
-
 if __name__ == "__main__":
     # basepath = "/Users/dheerajmekala/Work/Coarse2Fine/data/"
     basepath = "/data4/dheeraj/coarse2fine/"
@@ -101,12 +67,16 @@ if __name__ == "__main__":
     model.eval()
 
     parent_to_child = pickle.load(open(pkl_dump_dir + "parent_to_child.pkl", "rb"))
-    embeddings = {}
-    count = {}
+    label_embeddings = {}
 
-    embeddings, count = get_zihan_bert_embeddings(embeddings, count, model, tokenizer, list(df.text))
+    for p in parent_to_child:
+        embeddings = {}
+        count = {}
+        temp_df = df[df.label.isin([p])]
+        temp_df = temp_df.reset_index(drop=True)
+        embeddings, count = get_zihan_bert_embeddings(embeddings, count, model, tokenizer, list(temp_df.text))
+        for w in embeddings:
+            embeddings[w] = embeddings[w] / count[w]
+        label_embeddings[p] = embeddings
 
-    for w in embeddings:
-        embeddings[w] = embeddings[w] / count[w]
-
-    pickle.dump(embeddings, open(pkl_dump_dir + "bert_word_embeddings.pkl", "wb"))
+    pickle.dump(label_embeddings, open(pkl_dump_dir + "label_bert_word_embeddings.pkl", "wb"))
