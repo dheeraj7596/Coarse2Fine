ed9bfdea59af008aaddf2f734aec675197f1b59c
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-08-25 10:35:48 -0700

    cluster code added

--- cluster.py
+++ cluster.py
@@ -1,78 +1,110 @@
 import json
-from transformers import BertForSequenceClassification, BertTokenizer, BertModel
 import pickle
 import os
-import torch
 from util import cosine_similarity
 from scipy.special import softmax
 import numpy as np
 from util import print_seed_dict
 import sys
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "4"
-
-
-def get_bert_embeddings(model, tokenizer, words):
-    embeddings = {}
-    batch_word_ids = tokenizer(words, padding=True, truncation=True, return_tensors="pt")
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    batch_word_ids = batch_word_ids.to(device)
-    out = model(**batch_word_ids)
-    # hidden_states = out[1]
-    hidden_states = out[2]
-    sentence_embeddings = torch.mean(hidden_states[-1], dim=1)
-    for i, word in enumerate(words):
-        embeddings[word] = sentence_embeddings[i, :].detach().cpu().numpy()
-    return embeddings
+import string
+from nltk.corpus import stopwords
 
 
-def get_fine_seeds(child_labels, candidate_phrases, model, tokenizer, thresh=0.7):
-    child_label_embeddings = get_bert_embeddings(model, tokenizer, child_labels)
-    candidate_phrases_embeddings = get_bert_embeddings(model, tokenizer, candidate_phrases)
-
-    fine_seeds_dict = {}
+def get_arg_max_label(ph, child_labels, embeddings, threshold=0.0):
+    filtered_phrase = ph.translate(translator)
+    sims = []
     for ch in child_labels:
-        fine_seeds_dict[ch] = []
+        try:
+            child_label_str = " ".join([t for t in ch.split("_") if t not in stop_words]).strip()
+            sims.append(cosine_similarity(embeddings[filtered_phrase], embeddings[child_label_str]))
+        except Exception as e:
+            print("Error while computing cosine sim", e)
+            return None
+    sim_softmax = softmax(np.array(sims))
+    if max(sim_softmax) >= threshold:
+        max_ind = np.argmax(sim_softmax)
+        return child_labels[max_ind]
+    else:
+        return None
 
-    for cand_word in candidate_phrases_embeddings:
-        temp_sim = []
-        for ch in child_labels:
-            temp_sim.append(cosine_similarity(child_label_embeddings[ch], candidate_phrases_embeddings[cand_word]))
-        sim_softmax = softmax(np.array(temp_sim))
-        if max(sim_softmax) >= thresh:
-            max_ind = np.argmax(sim_softmax)
-            fine_seeds_dict[child_labels[max_ind]].append(cand_word)
-    return fine_seeds_dict
+
+def get_our_algo_arg_max(ph, child_labels, embeddings, mean_sim, threshold=0.0):
+    filtered_phrase = ph.translate(translator)
+    sims = []
+    for ch in child_labels:
+        try:
+            child_label_str = " ".join([t for t in ch.split("_") if t not in stop_words]).strip()
+            sims.append(cosine_similarity(embeddings[filtered_phrase], embeddings[child_label_str]) / mean_sim[ch])
+        except Exception as e:
+            print("Error while computing cosine sim", e)
+            return None
+    sim_softmax = softmax(np.array(sims))
+    if max(sim_softmax) >= threshold:
+        max_ind = np.argmax(sim_softmax)
+        return child_labels[max_ind]
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    # basepath = "/Users/dheerajmekala/Work/Coarse2Fine/data/"
-    basepath = "/data4/dheeraj/coarse2fine/"
+    basepath = "/Users/dheerajmekala/Work/Coarse2Fine/data/"
+    # basepath = "/data4/dheeraj/coarse2fine/"
     dataset = "nyt/"
     pkl_dump_dir = basepath + dataset
 
-    tok_path = pkl_dump_dir + "bert/tokenizer"
-    model_path = pkl_dump_dir + "bert/model"
+    # thresh = float(sys.argv[1])
+    # algo = int(sys.argv[2])
 
-    thresh = float(sys.argv[1])
+    thresh = 0.2
+    algo = 4
+
+    translator = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    stop_words = set(stopwords.words('english'))
+    stop_words.add('would')
+
+    embeddings = pickle.load(open(pkl_dump_dir + "bert_word_phrase_embeddings.pkl", "rb"))
     seed_phrases = json.load(open(pkl_dump_dir + "conwea_top100phrases.json", "r"))
-
-    # tokenizer = BertTokenizer.from_pretrained(tok_path)
-    # model = BertForSequenceClassification.from_pretrained(model_path)
-
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = model.to(device)
-    model.eval()
+    # mean_sim = json.load(open(pkl_dump_dir + "mean_sim.json", "r"))
+    mean_sim = json.load(open(pkl_dump_dir + "mean_sim_top100.json", "r"))
+    all_sims = json.load(open(pkl_dump_dir + "all_sims.json", "r"))
 
     parent_to_child = pickle.load(open(pkl_dump_dir + "parent_to_child.pkl", "rb"))
 
     child_seeds_dict = {}
+    general_seeds = []
+    all_child_labels = []
     for p in parent_to_child:
         child_labels = parent_to_child[p]
-        fine_seeds_dict = get_fine_seeds(child_labels, list(seed_phrases[p].keys()), model, tokenizer, thresh=thresh)
-        for c in child_labels:
-            child_seeds_dict[c] = fine_seeds_dict[c]
+        all_child_labels += child_labels
+        for ph in list(seed_phrases[p].keys()):
+            if algo == 1:
+                ch = get_arg_max_label(ph, child_labels, embeddings)
+            elif algo == 2:
+                ch = get_arg_max_label(ph, child_labels, embeddings, threshold=thresh)
+            elif algo == 3:
+                ch = get_our_algo_arg_max(ph, child_labels, embeddings, mean_sim)
+            elif algo == 4:
+                ch = get_our_algo_arg_max(ph, child_labels, embeddings, mean_sim, threshold=thresh)
+            else:
+                raise ValueError("algo should be in 1,2,3,4")
+
+            if ch is None:
+                general_seeds.append(ph)
+            else:
+                try:
+                    child_seeds_dict[ch].append(ph)
+                except:
+                    child_seeds_dict[ch] = [ph]
 
     print_seed_dict(child_seeds_dict)
+    print("Missing seeds for: ", set(all_child_labels) - set(child_seeds_dict.keys()))
+    print("General Seeds: ", general_seeds)
+
+    # json.dump(child_seeds_dict, open(pkl_dump_dir + "child_seeds_dict_argmax.json", "w"))
+    # json.dump(child_seeds_dict, open(pkl_dump_dir + "child_seeds_dict_argmax_thresh0.7.json", "w"))
+
+    # json.dump(child_seeds_dict, open(pkl_dump_dir + "child_seeds_dict_ouralgo_argmax.json", "w"))
+    # json.dump(child_seeds_dict, open(pkl_dump_dir + "child_seeds_dict_ouralgo_argmax_thresh0.2.json", "w"))
+
+    # json.dump(child_seeds_dict, open(pkl_dump_dir + "child_seeds_dict_ouralgo_argmax_meansim_100.json", "w"))
+    json.dump(child_seeds_dict, open(pkl_dump_dir + "child_seeds_dict_ouralgo_argmax_meansim_100_thresh0.2.json", "w"))
