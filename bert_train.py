5939bd7194ff34b4e59e06d1363b309a9201262d
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-10-28 17:01:35 -0700

    bug fixed.

--- bert_train.py
+++ bert_train.py
@@ -111,8 +111,6 @@ def train(train_dataloader, validation_dataloader, device, num_labels):
         output_attentions=False,  # Whether the model returns attentions weights.
         output_hidden_states=False,  # Whether the model returns all hidden-states.
     )
-    if device == torch.device("cuda"):
-        model.cuda()
 
     # Note: AdamW is a class from the huggingface library (as opposed to pytorch)
     # I believe the 'W' stands for 'Weight Decay fix"
@@ -450,4 +448,5 @@ if __name__ == "__main__":
         device = torch.device("cpu")
 
     model = train(train_dataloader, validation_dataloader, device, num_labels=len(label_to_index))
+    model.to(device)
     test(df_test, label_to_index, index_to_label)
