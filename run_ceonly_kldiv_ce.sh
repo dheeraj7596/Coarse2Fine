fc1705d27aa777dc1b0d8f19f5d1dcca0f7b0d32
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-01-02 22:52:02 -0800

    fixed bug

--- run_ceonly_kldiv_ce.sh
+++ run_ceonly_kldiv_ce.sh
@@ -1,14 +1,15 @@
 gpu=$1
 dataset=$2
+num=$3
 echo "ITERATION-1"
 echo "Running GPT2 CE-only iteration 1"
 python3 gpt2_fine_ceonly_ablation.py 1 ${gpu} 1 ${dataset}
 echo "Running GPT2 Fine-tune iteration 1"
 python3 gpt2_finetune_politics_prestart.py 1 ${gpu} ${gpu} ${dataset} 1
 echo "Generating Data.."
-python3 generate_data.py 1 ${gpu} politics 250
+python3 generate_data.py 1 ${gpu} ${dataset} ${num}
 echo "Training BERT.."
-python3 bert_train.py 1 ${gpu} 1 politics 1
+python3 bert_train.py 1 ${gpu} 1 ${dataset} 1
 echo "***********************************************************************"
 echo "ITERATION-2"
 echo "Running GPT2 CE-only iteration 2"
@@ -16,9 +17,9 @@ python3 gpt2_fine_ceonly_ablation.py 1 ${gpu} 2 ${dataset}
 echo "Running GPT2 Fine-tune iteration 2"
 python3 gpt2_finetune_politics_prestart.py 1 ${gpu} ${gpu} ${dataset} 2
 echo "Generating Data.."
-python3 generate_data.py 1 ${gpu} politics 250
+python3 generate_data.py 1 ${gpu} ${dataset} ${num}
 echo "Training BERT.."
-python3 bert_train.py 1 ${gpu} 2 politics 1
+python3 bert_train.py 1 ${gpu} 2 ${dataset} 1
 echo "***********************************************************************"
 echo "ITERATION-3"
 echo "Running GPT2 CE-only iteration 3"
@@ -26,6 +27,6 @@ python3 gpt2_fine_ceonly_ablation.py 1 ${gpu} 3 ${dataset}
 echo "Running GPT2 Fine-tune iteration 3"
 python3 gpt2_finetune_politics_prestart.py 1 ${gpu} ${gpu} ${dataset} 3
 echo "Generating Data.."
-python3 generate_data.py 1 ${gpu} politics 250
+python3 generate_data.py 1 ${gpu} ${dataset} ${num}
 echo "Training BERT.."
-python3 bert_train.py 1 ${gpu} 3 politics 1
+python3 bert_train.py 1 ${gpu} 3 ${dataset} 1
