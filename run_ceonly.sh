50cdd5ae6ce86fb4ff96bc0bb482654a9b5420de
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-01-04 10:21:08 -0800

    modified threshold computation and added shell files

---
+++ run_ceonly.sh
@@ -0,0 +1,26 @@
+gpu=$1
+dataset=$2
+num=$3
+echo "ITERATION-1"
+echo "Running GPT2 CE-only iteration 1"
+python3 gpt2_fine_ceonly_ablation.py 1 ${gpu} 1 ${dataset}
+echo "Generating Data.."
+python3 generate_data.py 1 ${gpu} ${dataset} ${num}
+echo "Training BERT.."
+python3 bert_train_ceonly.py 1 ${gpu} 1 ${dataset} 1
+echo "***********************************************************************"
+echo "ITERATION-2"
+echo "Running GPT2 CE-only iteration 2"
+python3 gpt2_fine_ceonly_ablation.py 1 ${gpu} 2 ${dataset}
+echo "Generating Data.."
+python3 generate_data.py 1 ${gpu} ${dataset} ${num}
+echo "Training BERT.."
+python3 bert_train_ceonly.py 1 ${gpu} 2 ${dataset} 1
+echo "***********************************************************************"
+echo "ITERATION-3"
+echo "Running GPT2 CE-only iteration 3"
+python3 gpt2_fine_ceonly_ablation.py 1 ${gpu} 3 ${dataset}
+echo "Generating Data.."
+python3 generate_data.py 1 ${gpu} ${dataset} ${num}
+echo "Training BERT.."
+python3 bert_train_ceonly.py 1 ${gpu} 3 ${dataset} 1
