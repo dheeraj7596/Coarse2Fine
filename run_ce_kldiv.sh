c9d51e9c006ce3b11a7cb380aa5da0051386e0bc
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-01-05 12:09:02 -0800

    shell script for ce_kldiv

---
+++ run_ce_kldiv.sh
@@ -0,0 +1,27 @@
+gpu=$1
+plabel=$2
+num=$3
+dataset=$4
+echo "ITERATION-1"
+echo "Running GPT2 CE-KL-div iteration 1"
+python3 gpt_finetune_ce_kl_div.py 1 ${gpu} 1 ${plabel} fine ${dataset}
+echo "Generating Data.."
+python3 generate_data.py 1 ${gpu} ${plabel} ${num} fine ${dataset}
+echo "Training BERT.."
+python3 bert_train.py 1 ${gpu} 1 ${plabel} 1 ${dataset}
+echo "***********************************************************************"
+echo "ITERATION-2"
+echo "Running GPT2 CE-KL-div iteration 2"
+python3 gpt_finetune_ce_kl_div.py 1 ${gpu} 2 ${plabel} fine ${dataset}
+echo "Generating Data.."
+python3 generate_data.py 1 ${gpu} ${plabel} ${num} fine ${dataset}
+echo "Training BERT.."
+python3 bert_train.py 1 ${gpu} 2 ${plabel} 1 ${dataset}
+echo "***********************************************************************"
+echo "ITERATION-3"
+echo "Running GPT2 CE-KL-div iteration 3"
+python3 gpt_finetune_ce_kl_div.py 1 ${gpu} 3 ${plabel} fine ${dataset}
+echo "Generating Data.."
+python3 generate_data.py 1 ${gpu} ${plabel} ${num} fine ${dataset}
+echo "Training BERT.."
+python3 bert_train.py 1 ${gpu} 3 ${plabel} 1 ${dataset}
