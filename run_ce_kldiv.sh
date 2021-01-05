6d2ccea62550f68eb77248c0fbb2f0f194f17c1b
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-01-05 12:11:59 -0800

    minor fix

--- run_ce_kldiv.sh
+++ run_ce_kldiv.sh
@@ -4,7 +4,7 @@ num=$3
 dataset=$4
 echo "ITERATION-1"
 echo "Running GPT2 CE-KL-div iteration 1"
-python3 gpt_finetune_ce_kl_div.py 1 ${gpu} 1 ${plabel} fine ${dataset}
+python3 gpt2_finetune_ce_kl_div.py.py 1 ${gpu} 1 ${plabel} fine ${dataset}
 echo "Generating Data.."
 python3 generate_data.py 1 ${gpu} ${plabel} ${num} fine ${dataset}
 echo "Training BERT.."
@@ -12,7 +12,7 @@ python3 bert_train.py 1 ${gpu} 1 ${plabel} 1 ${dataset}
 echo "***********************************************************************"
 echo "ITERATION-2"
 echo "Running GPT2 CE-KL-div iteration 2"
-python3 gpt_finetune_ce_kl_div.py 1 ${gpu} 2 ${plabel} fine ${dataset}
+python3 gpt2_finetune_ce_kl_div.py.py 1 ${gpu} 2 ${plabel} fine ${dataset}
 echo "Generating Data.."
 python3 generate_data.py 1 ${gpu} ${plabel} ${num} fine ${dataset}
 echo "Training BERT.."
@@ -20,7 +20,7 @@ python3 bert_train.py 1 ${gpu} 2 ${plabel} 1 ${dataset}
 echo "***********************************************************************"
 echo "ITERATION-3"
 echo "Running GPT2 CE-KL-div iteration 3"
-python3 gpt_finetune_ce_kl_div.py 1 ${gpu} 3 ${plabel} fine ${dataset}
+python3 gpt2_finetune_ce_kl_div.py.py 1 ${gpu} 3 ${plabel} fine ${dataset}
 echo "Generating Data.."
 python3 generate_data.py 1 ${gpu} ${plabel} ${num} fine ${dataset}
 echo "Training BERT.."
