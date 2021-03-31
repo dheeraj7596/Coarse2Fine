282504161afb2af77a4f5d8213e9305cfdf94b33
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-03-31 09:44:08 -0700

    Dumping bert model preds part-2

--- run_ce_20news.sh
+++ run_ce_20news.sh
@@ -1,48 +1,48 @@
 gpu=$1
-echo "ITERATION-1"
-echo "Running GPT2 CE iteration 1"
-python3 gpt2_ce.py 1 ${gpu} 1 20news ce
-echo "Generating Computer Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} computer 2000 20news ce
-echo "Training Computer BERT.."
-python3 bert_train.py 1 ${gpu} 1 computer 1 20news ce
-echo "Generating Recreation Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} recreation 2000 20news ce
-echo "Training Recreation BERT.."
-python3 bert_train.py 1 ${gpu} 1 recreation 1 20news ce
-echo "Generating Science Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} science 2000 20news ce
-echo "Training Science BERT.."
-python3 bert_train.py 1 ${gpu} 1 science 1 20news ce
-echo "Generating Politics Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} politics 2000 20news ce
-echo "Training Politics BERT.."
-python3 bert_train.py 1 ${gpu} 1 politics 1 20news ce
-echo "Generating Religion Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} religion 2000 20news ce
-echo "Training Religion BERT.."
-python3 bert_train.py 1 ${gpu} 1 religion 1 20news ce
-echo "***********************************************************************"
+#echo "ITERATION-1"
+#echo "Running GPT2 CE iteration 1"
+#python3 gpt2_ce.py 1 ${gpu} 1 20news ce
+#echo "Generating Computer Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} computer 2000 20news ce
+#echo "Training Computer BERT.."
+#python3 bert_train.py 1 ${gpu} 1 computer 1 20news ce
+#echo "Generating Recreation Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} recreation 2000 20news ce
+#echo "Training Recreation BERT.."
+#python3 bert_train.py 1 ${gpu} 1 recreation 1 20news ce
+#echo "Generating Science Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} science 2000 20news ce
+#echo "Training Science BERT.."
+#python3 bert_train.py 1 ${gpu} 1 science 1 20news ce
+#echo "Generating Politics Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} politics 2000 20news ce
+#echo "Training Politics BERT.."
+#python3 bert_train.py 1 ${gpu} 1 politics 1 20news ce
+#echo "Generating Religion Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} religion 2000 20news ce
+#echo "Training Religion BERT.."
+#python3 bert_train.py 1 ${gpu} 1 religion 1 20news ce
+#echo "***********************************************************************"
 echo "ITERATION-2"
-echo "Running GPT2 CE iteration 2"
-python3 gpt2_ce.py 1 ${gpu} 2 20news ce
-echo "Generating Computer Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} computer 2000 20news ce
+#echo "Running GPT2 CE iteration 2"
+#python3 gpt2_ce.py 1 ${gpu} 2 20news ce
+#echo "Generating Computer Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} computer 2000 20news ce
 echo "Training Computer BERT.."
 python3 bert_train.py 1 ${gpu} 2 computer 1 20news ce
-echo "Generating Recreation Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} recreation 2000 20news ce
+#echo "Generating Recreation Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} recreation 2000 20news ce
 echo "Training Recreation BERT.."
 python3 bert_train.py 1 ${gpu} 2 recreation 1 20news ce
-echo "Generating Science Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} science 2000 20news ce
+#echo "Generating Science Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} science 2000 20news ce
 echo "Training Science BERT.."
 python3 bert_train.py 1 ${gpu} 2 science 1 20news ce
-echo "Generating Politics Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} politics 2000 20news ce
+#echo "Generating Politics Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} politics 2000 20news ce
 echo "Training Politics BERT.."
 python3 bert_train.py 1 ${gpu} 2 politics 1 20news ce
-echo "Generating Religion Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} religion 2000 20news ce
+#echo "Generating Religion Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} religion 2000 20news ce
 echo "Training Religion BERT.."
 python3 bert_train.py 1 ${gpu} 2 religion 1 20news ce
