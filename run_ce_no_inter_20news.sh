6f2bfdd62a4d4cb7b1a272689dba0f93e94fb060
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-05-10 23:55:34 -0700

    added shell script for no-inter no hinge 20news

--- run_ce_no_inter_20news.sh
+++ run_ce_no_inter_20news.sh
@@ -1,47 +1,63 @@
 gpu=$1
 echo "ITERATION-1"
-echo "Running GPT2 CE iteration 1"
-python3 gpt2_ce.py 1 ${gpu} 1 20news ce
+echo "Running GPT2 CE computer iteration 1"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 1 20news ce computer
 echo "Generating Computer Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} computer 2000 20news ce
 echo "Training Computer BERT.."
 python3 bert_train.py 1 ${gpu} 1 computer 1 20news ce
+echo "Running GPT2 CE recreation iteration 1"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 1 20news ce recreation
 echo "Generating Recreation Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} recreation 2000 20news ce
 echo "Training Recreation BERT.."
 python3 bert_train.py 1 ${gpu} 1 recreation 1 20news ce
+echo "Running GPT2 CE science iteration 1"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 1 20news ce science
 echo "Generating Science Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} science 2000 20news ce
 echo "Training Science BERT.."
 python3 bert_train.py 1 ${gpu} 1 science 1 20news ce
+echo "Running GPT2 CE politics iteration 1"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 1 20news ce politics
 echo "Generating Politics Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} politics 2000 20news ce
 echo "Training Politics BERT.."
 python3 bert_train.py 1 ${gpu} 1 politics 1 20news ce
+echo "Running GPT2 CE religion iteration 1"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 1 20news ce religion
 echo "Generating Religion Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} religion 2000 20news ce
 echo "Training Religion BERT.."
 python3 bert_train.py 1 ${gpu} 1 religion 1 20news ce
 echo "***********************************************************************"
 echo "ITERATION-2"
-echo "Running GPT2 CE iteration 2"
-python3 gpt2_ce.py 1 ${gpu} 2 20news ce
+echo "Running GPT2 CE computer iteration 2"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 2 20news ce computer
 echo "Generating Computer Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} computer 2000 20news ce
 echo "Training Computer BERT.."
 python3 bert_train.py 1 ${gpu} 2 computer 1 20news ce
+echo "Running GPT2 CE recreation iteration 2"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 2 20news ce recreation
 echo "Generating Recreation Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} recreation 2000 20news ce
 echo "Training Recreation BERT.."
 python3 bert_train.py 1 ${gpu} 2 recreation 1 20news ce
+echo "Running GPT2 CE science iteration 2"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 2 20news ce science
 echo "Generating Science Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} science 2000 20news ce
 echo "Training Science BERT.."
 python3 bert_train.py 1 ${gpu} 2 science 1 20news ce
+echo "Running GPT2 CE politics iteration 2"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 2 20news ce politics
 echo "Generating Politics Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} politics 2000 20news ce
 echo "Training Politics BERT.."
 python3 bert_train.py 1 ${gpu} 2 politics 1 20news ce
+echo "Running GPT2 CE religion iteration 2"
+python3 gpt2_ce_no_inter.py 1 ${gpu} 2 20news ce religion
 echo "Generating Religion Data.."
 python3 generate_data_ce_hinge.py 1 ${gpu} religion 2000 20news ce
 echo "Training Religion BERT.."
