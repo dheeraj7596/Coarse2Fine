28439f00c52680128bcab8e6c84c555dd481d6c0
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-07-14 13:43:34 -0700

    commented the data generation for arts part

--- run_ce_hinge_gpt.sh
+++ run_ce_hinge_gpt.sh
@@ -1,9 +1,9 @@
 gpu=$1
-echo "ITERATION-1"
-echo "Running GPT CE-Hinge iteration 1"
-python3 gpt_ce_hinge.py 1 ${gpu} 1 nyt ce_hinge
-echo "Generating Arts Data.."
-python3 generate_data_ce_hinge.py 1 ${gpu} arts 500 nyt ce_hinge
+#echo "ITERATION-1"
+#echo "Running GPT CE-Hinge iteration 1"
+#python3 gpt_ce_hinge.py 1 ${gpu} 1 nyt ce_hinge
+#echo "Generating Arts Data.."
+#python3 generate_data_ce_hinge.py 1 ${gpu} arts 500 nyt ce_hinge
 echo "Training Arts BERT.."
 python3 bert_train.py 1 ${gpu} 1 arts 1 nyt ce_hinge
 echo "Generating Politics Data.."
